9cce9f394f835bf2a1b36a208b27ff1799ea590e
David SPORN <sporniket.studio@gmail.com>
2020-03-27 23:03:57 +0100

    addresses #4 : Use a database to store the catalog

--- src/test/java/integration/api/catalog/products/ProdcutsByIdIT.java
+++ src/test/java/integration/api/catalog/products/ProdcutsByIdIT.java
@@ -6,49 +6,59 @@ import static org.assertj.core.api.BDDAssertions.then;
 import javax.annotation.PostConstruct;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
-import com.sporniket.littlecauldron.api.catalog.ApiCatalogApplication;
 import com.sporniket.littlecauldron.api.catalog.ResourceError;
 import com.sporniket.littlecauldron.api.catalog.ResourceProduct;
+import com.sporniket.littlecauldron.api.catalog.ResourceProducts;
 import com.sporniket.littlecauldron.utils.JsonHelper;
 
-@SpringBootTest(classes = ApiCatalogApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ProdcutsByIdIT
+import integration.IntegrationTest;
+
+public class ProdcutsByIdIT extends IntegrationTest
 {
+	private static final String FORMAT_REST_CALL_PATH_PRODUCT_BY_ID = "/catalog/products/%s";
+
+	private static final String FORMAT_REST_CALL_PATH_PRODUCTS = "/catalog/products";
+
 	private static final String FORMAT_REST_CALL_URL = "http://localhost:%s%s";
 
-	private static final String FORMAT_REST_CALL_PATH = "/catalog/products/%s";
+	private String restCallPathProductById;
 
-	@LocalServerPort
-	int randomServerPort;
-
-	private String restCallPath;
+	private String restCallPathProducts;
 
 	private TestRestTemplate restTemplate = new TestRestTemplate();
 
 	@PostConstruct
 	public void finishInitialization()
 	{
-		restCallPath = format(FORMAT_REST_CALL_URL, randomServerPort, FORMAT_REST_CALL_PATH);
+		restCallPathProductById = format(FORMAT_REST_CALL_URL, randomServerPort, FORMAT_REST_CALL_PATH_PRODUCT_BY_ID);
+		restCallPathProducts = format(FORMAT_REST_CALL_URL, randomServerPort, FORMAT_REST_CALL_PATH_PRODUCTS);
 	}
 
-	private ResponseEntity<String> performCall(String idProduct)
+	private ResponseEntity<String> performCall()
 	{
-		final String effectiveCall = format(restCallPath, idProduct);
+		final String effectiveCall = restCallPathProducts;
+		return restTemplate.getForEntity(effectiveCall, String.class);
+	}
+
+	private ResponseEntity<String> performCallById(String idProduct)
+	{
+		final String effectiveCall = format(restCallPathProductById, idProduct);
 		return restTemplate.getForEntity(effectiveCall, String.class);
 	}
 
 	@Test
-	public void shouldReturnExistingProductData() throws Exception
+	public void shouldReturnAllExistingProductData() throws Exception
 	{
-		ResponseEntity<String> response = performCall("LC01");
+		ResponseEntity<String> response = performCall();
 
 		then(response.getStatusCode().value()).isEqualTo(200);
-		ResourceProduct actualProduct = JsonHelper.objectFrom(response.getBody(), ResourceProduct.class);
+		ResourceProducts actualProducts = JsonHelper.objectFrom(response.getBody(), ResourceProducts.class);
+		then(actualProducts.getProducts()).isNotEmpty();
+		then(actualProducts.getProducts().size()).isEqualTo(1);
+		ResourceProduct actualProduct = actualProducts.getProducts().get(0);
 		then(actualProduct.getId()).isEqualTo("LC01");
 		then(actualProduct.getLabel()).isEqualTo("Standard Quality");
 	}
@@ -56,11 +66,22 @@ public class ProdcutsByIdIT
 	@Test
 	public void shouldReturnErrorOnProductNotFound() throws Exception
 	{
-		ResponseEntity<String> response = performCall("WHATEVER");
+		ResponseEntity<String> response = performCallById("WHATEVER");
 
 		then(response.getStatusCode().value()).isEqualTo(404);
 		ResourceError actualError = JsonHelper.objectFrom(response.getBody(), ResourceError.class);
 		then(actualError.getError()).isEqualTo("1001");
 		then(actualError.getReason()).isEqualTo("Product 'WHATEVER' not found.");
+	}
+
+	@Test
+	public void shouldReturnExistingProductData() throws Exception
+	{
+		ResponseEntity<String> response = performCallById("LC01");
+
+		then(response.getStatusCode().value()).isEqualTo(200);
+		ResourceProduct actualProduct = JsonHelper.objectFrom(response.getBody(), ResourceProduct.class);
+		then(actualProduct.getId()).isEqualTo("LC01");
+		then(actualProduct.getLabel()).isEqualTo("Standard Quality");
 	}
 }
